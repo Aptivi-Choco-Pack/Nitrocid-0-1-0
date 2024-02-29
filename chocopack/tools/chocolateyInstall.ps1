7ace6f74b2d46b4f09bcf28eb071641685f16114
Aptivi <eoflaoevicecity@gmail.com>
2024-02-29 12:49:48 +0300

    Add project files.

---
+++ chocopack/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pkgName    = "KS"
+$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0/0.1.0-bin.zip"
+$md5check   = "62fc5d7e60978ed45961f2009b1ce82e"
+
+Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
+Write-Output "<*> Installation directory: $toolsDir"
+Write-Output "<*> Package Name: $pkgName"
+Write-Output "<*> URL: $url"
+Write-Output "<*> Expected MD5 Sum: $md5check"
+Write-Output "<+> Configuration will be automatically generated on startup."
+
+Install-ChocolateyZipPackage $pkgName $url $toolsDir -ChecksumType "md5" -Checksum $md5check
